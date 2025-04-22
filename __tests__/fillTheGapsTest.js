406e07a002c5ee33f32c61af1a45d503eafadb1a
kiwylai <kiwylai@gmail.com>
2025-04-22 12:31:15 +0200

    finish the test

--- __tests__/fillTheGapsTest.js
+++ __tests__/fillTheGapsTest.js
@@ -6,6 +6,10 @@ describe.each`
     ${[1, null, null, null, 1]} | ${[1, 1, 1, 1, 1]}
     ${[1, null, 1, 2, null, 2]} | ${[1, 1, 1, 2, 2, 2]}
     ${[1, null, 2, null, 2, null, 1]} | ${[1, null, 2, 2, 2, null, 1]}
+    ${[1, null, 2]} | ${[1, null, 2]}
+    ${[null, 1, null]} | ${[null, 1, null]}
+    ${['codewars', null, null, 'codewars', 'real work', null, null, 'real work']} | 
+    ${["codewars", "codewars", "codewars", "codewars", "real work", "real work", "real work", "real work"]}
 `("fill_gaps($input) should return $expected", ({ input, expected }) => {
     test("", () => {
         expect(fill_gaps(input)).toEqual(expected);
--- production/fillTheGaps.js
+++ production/fillTheGaps.js
@@ -1,12 +1,26 @@
 function fill_gaps(inputArray) {
+    // [1, null, 2, null, 2, null, 1]
+    function getNameOfClosedBracket(startIndex){
+        for (let i = startIndex; i < inputArray.length; i++){
+            let currentValue = inputArray[i];
+            if (currentValue !== null){
+                return currentValue;
+            }
+        } 
+        return null;
+    }
     let outputArray = [];
     let bracketName = undefined;
     for (let i = 0; i < inputArray.length; i++) {
         let currentValue = inputArray[i];
         if (currentValue !== null) {
             bracketName = currentValue;
+            outputArray.push(bracketName);
+        } else {
+            let closedBracketName = getNameOfClosedBracket(i);
+            outputArray.push(
+                closedBracketName !== bracketName? currentValue : bracketName);
         }
-        outputArray.push(bracketName);
     }
     return outputArray;
 }
