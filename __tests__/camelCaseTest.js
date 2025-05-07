da3a3a0d0c05c75c0c4489cbcaf86fa4837b6d8e
OvisMaximus <OvisMaximus@users.noreply.github.com>
2025-05-07 10:53:12 +0200

    get the challenge of CamelCase

--- __tests__/camelCaseTest.js
+++ __tests__/camelCaseTest.js
@@ -1,5 +1,22 @@
 // https://www.codewars.com/kata/58097ae96037b88f57000105
+const Test = require('@codewars/test-compat');
 
+describe("Basic Tests", function(){
+    it("It should works for basic tests", function(){
 
+        Test.assertSimilar(toCamelCase("hello world",1) , "helloWorld")
 
-module.exports = ;
+//        Test.assertSimilar(toCamelCase("hello world",2) , "hellOworld")
+
+//        Test.assertSimilar(toCamelCase("hello world",3) , "hellOWorld")
+
+//        Test.assertSimilar(toCamelCase("Hello world",1) , "helloWorld")
+
+//        Test.assertSimilar(toCamelCase("Each number plus one",1) , "eachNumberPlusOne")
+
+//        Test.assertSimilar(toCamelCase("Each number plus one",2) , "eacHnumbeRpluSone")
+
+//        Test.assertSimilar(toCamelCase("Each number plus one",3) , "eacHNumbeRPluSOne")
+
+    })
+})
